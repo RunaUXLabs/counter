d56cf6edea4082f60fd4d606a67299f70c97138f
Runa <runa.uxlabs@gmail.com>
2024-11-29 11:31:20 +0900

    주석추가

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,18 @@ import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
+  // useState 훅을 가져와 초기값 0으로 설정한 다음에 구조분해할당
+  // count와 setCount()로 분리함
 
   return (
     <>
       <h1>Counter</h1>
       <p>{count}</p>
+      {/* 자식이 부모로부터 props로 값을 전달받음 */}
+
       <button onClick={() => setCount(count + 1)}> 증가 </button>
       <button onClick={() => setCount(count - 1)}> 감소 </button>
+      {/* 버튼을 클릭하면 부모의 setCount()실행시킴 => 해당함수가 부모의 상태값을 바꿈 */}
     </>
   );
 }
--- src/CounterButton.jsx
+++ src/CounterButton.jsx
@@ -1,17 +1,17 @@
 // 버튼 역할을 담당하는 재사용 가능한 자식 컴포넌트
 // eslint-disable-next-line react/prop-types
-function CounterButton({ label, func, style }) {
+function CounterButton({ label, func }) {
   // 초심자를 위한 props반영 설명
   return (
-    <button onClick={func} style={style}>
+    <button onClick={func}>
       {label}
     </button>
   );
 }
-// function CounterButton({ label, onClick, style }) {
+// function CounterButton({ label, onClick }) {
 //   // 보통은 이렇게 씀
 //   return (
-//     <button onClick={onClick} style={style}>
+//     <button onClick={onClick}>
 //       {label}
 //     </button>
 //   );
