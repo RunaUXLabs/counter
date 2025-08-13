9b779fc2874966f7d1fe02b81efd2728e653b5f0
Runa <runa.uxlabs@gmail.com>
2025-08-13 10:54:13 +0900

    주석 수정

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,19 @@ function App() {
   const [count, setCount] = useState(0);
   // useState 훅을 가져와 초기값 0으로 설정한 다음에 구조분해할당
   // count와 setCount()로 분리함
-  console.log(useState(0));
-  console.log(count);
+  // console.log(useState(0));
+  // console.log(count);
 
 
   return (
     <>
       <h1>초심플 카운터</h1>
       <p>{count}</p>
-      {/* 자식이 부모로부터 props로 값을 전달받음 */}
+      {/* 선언된 변수 표현식으로 출력 */}
 
       <button onClick={() => setCount(count + 1)}> 증가 </button>
       <button onClick={() => setCount(count - 1)}> 감소 </button>
+      {/* 자식이 부모로부터 props로 값을 전달받음 */}
       {/* 버튼을 클릭하면 부모의 setCount()실행시킴 => 해당함수가 부모의 상태값을 바꿈 */}
     </>
   );
